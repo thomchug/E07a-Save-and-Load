e57564990eeef76c4c374d7af3a1b328aa767a3e
thomchug <59849813+thomchug@users.noreply.github.com>
2020-02-26 16:19:04 -0500

    Completed Exercise

--- Scenes/SaveSystem.gd
+++ Scenes/SaveSystem.gd
@@ -1,12 +1,15 @@
 extends Node2D
 
 var DisplayValue = 0
+var save_pathway = "res://save-file.cfg"
+var config = ConfigFile.new()
+var load_response = config.load(save_pathway)
 
 func _ready():
-	pass 
+	pass
 
-func saveValue():
-	pass
-	
-func loadValue():
-	pass
+func saveValue(section, key):
+	config.set_value(section, key, DisplayValue)
+	config.save(save_pathway)
+func loadValue(section, key):
+	DisplayValue = config.get_value(section, key, DisplayValue)
